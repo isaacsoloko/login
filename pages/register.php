0d70b467bb564d49a9a602b230eb4313d5050e0f
Winner <isaacsoloko@gmail.com>
2023-08-25 22:02:26 +0100

    Prise en charge de og:image

--- pages/register.php
+++ pages/register.php
@@ -25,7 +25,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content="Login System" />
         <meta property="og:type" content="profile" />
-        <meta property="og:image" content="https://isaacsoloko.github.io/to-do-list/img/code.png" />
+        <meta property="og:image" content="https://isaacsoloko.github.io/login/img/code.png" />
         <meta property="og:description" content="Php :: Mise en pratique des notions acquises" />
         <link rel="shortcut icon" href="../img/code.png" type="image/x-icon" />
         <link rel="stylesheet" href="../css/style.css" />
